44dd58872cb6a0cfb0d3ca099e33deae4c8cb0ec
Anderson <andersonjoseph@mailfence.com>
2024-09-04 15:26:35 -0400

    rename get audio file handler

--- internal/audio/handle-get-audio-file.go
+++ internal/audio/handle-get-audio-file.go
@@ -2,60 +2,49 @@ package audio
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
-	"io/fs"
-	"log/slog"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/andersonjoseph/soundgo/internal/api"
 	"github.com/andersonjoseph/soundgo/internal/audio/audiorange"
+	"github.com/andersonjoseph/soundgo/internal/shared"
 )
 
-type audioFile struct {
-	File *os.File
-	Info fs.FileInfo
-}
-
-// GET /audios/{id}
-func (h Handler) GetAudio(ctx context.Context, params api.GetAudioParams) (api.GetAudioRes, error) {
-	audioFile, err := getAudioFile()
+// GET /audios/{id}/file
+func (h Handler) GetAudioFile(ctx context.Context, params api.GetAudioFileParams) (api.GetAudioFileRes, error) {
+	audioFile, err := h.fileRepository.Get(ctx, params.ID)
+	if errors.Is(err, shared.ErrNotFound) {
+		return &api.GetAudioFileNotFound{}, nil
+	}
 	if err != nil {
 		return nil, err
 	}
 
-	var res api.GetAudioRes
-	h.logger.Info("getting audio", slog.Group(
-		"input",
-		"ID",
-		params.ID,
-		"range",
-		params.Range.Value,
-	))
+	var res api.GetAudioFileRes
 
 	if params.Range.IsSet() {
 		p := audiorange.Parser{}
 		parsedRanges, err := p.Parse(params.Range.Value)
 		if err != nil {
-			return &api.GetAudioRequestedRangeNotSatisfiable{
+			return &api.GetAudioFileRequestedRangeNotSatisfiable{
 				Error: api.NewOptString(err.Error()),
 			}, nil
 		}
 
 		partialFile, err := getPartialFile(audioFile, parsedRanges[0])
 
-		res = &api.GetAudioPartialContentHeaders{
-			ContentRange: api.NewOptString(getContentRangeString(parsedRanges[0], audioFile.Info.Size())),
-			Response: api.GetAudioPartialContent{
+		res = &api.GetAudioFilePartialContentHeaders{
+			ContentRange: api.NewOptString(getContentRangeString(parsedRanges[0], audioFile.Size)),
+			Response: api.GetAudioFilePartialContent{
 				Data: partialFile,
 			},
 		}
 	} else {
-		res = &api.GetAudioOKHeaders{
+		res = &api.GetAudioFileOKHeaders{
 			AcceptRanges: api.NewOptString("bytes"),
-			Response: api.GetAudioOK{
+			Response: api.GetAudioFileOK{
 				Data: audioFile.File,
 			},
 		}
@@ -64,7 +53,7 @@ func (h Handler) GetAudio(ctx context.Context, params api.GetAudioParams) (api.G
 	return res, nil
 }
 
-func getPartialFile(f audioFile, fRange audiorange.Range) (io.Reader, error) {
+func getPartialFile(f File, fRange audiorange.Range) (io.Reader, error) {
 	var seekPosition int
 	var offset int64
 
@@ -82,32 +71,10 @@ func getPartialFile(f audioFile, fRange audiorange.Range) (io.Reader, error) {
 
 	var limit = fRange.End
 	if limit == -1 {
-		limit = f.Info.Size() - 1
+		limit = f.Size - 1
 	}
 
 	return io.LimitReader(f.File, limit), nil
-}
-
-func getAudioFile() (audioFile, error) {
-	path, err := filepath.Abs("audios/audio.m4a")
-	if err != nil {
-		return audioFile{}, fmt.Errorf("error while building path: %w", err)
-	}
-
-	file, err := os.Open(path)
-	if err != nil {
-		return audioFile{}, fmt.Errorf("error while opening file: %w", err)
-	}
-
-	info, err := file.Stat()
-	if err != nil {
-		return audioFile{}, fmt.Errorf("error while getting file info: %w", err)
-	}
-
-	return audioFile{
-		File: file,
-		Info: info,
-	}, nil
 }
 
 func getContentRangeString(r audiorange.Range, size int64) string {
